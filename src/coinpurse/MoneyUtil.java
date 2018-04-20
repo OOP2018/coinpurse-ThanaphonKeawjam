13e439a4a2a38e3d53667108265d96ffd1dbc060
ThanaphonKeawjam <tongasura001@gmail.com>
2018-04-20 14:43:00 +0700

    fix MoneyUtil

--- src/coinpurse/MoneyUtil.java
+++ src/coinpurse/MoneyUtil.java
@@ -1,6 +1,7 @@
 package coinpurse;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
@@ -12,53 +13,16 @@ import java.util.List;
  *
  */
 public class MoneyUtil {
-
-	/**
-	 * add value and currency to value list.
-	 */
-	public static void sortCoins(){
-		List<Valuable> coins = new ArrayList<Valuable>();
-		coins.add(new Coin(10.0, "Baht"));
-		coins.add(new Coin(5.0, "Baht"));
-		coins.add(new Coin(5.0, "Baht"));
-		coins.add(new Coin(0.5, "Baht"));
-		coins.add(new Coin(20.0, "USD"));
-		coins.add(new Coin(20.0, "Peso"));
-		coins.add(new Coin(1000.0, "USD"));
-		coins.add(new Coin(100.0, "USD"));
-		coins.add(new Coin(100.0, "Yen"));
-		coins.add(new Coin(100.0, "Peso"));
-		coins.add(new Coin(50.0, "Yen"));
-		coins.add(new Coin(50.0, "Yuan"));
-		coins.add(new Coin(120.0, "Yuan"));
-		coins.add(new BankNote(200.0, "Yen", 1000000));
-		coins.add(new BankNote(500.0, "Yen", 1000001));
-		
-		printCoins(coins);
-		System.out.println("==========");
-		sortCoins(coins);
-		System.out.println("=========");
-		System.out.println(filterByCurrency(coins, "USD"));
-	}
 	
 	/**
-	 * Print list of value.
-	 * @param coins is list of value.
-	 */
-	public static void printCoins(List<Valuable> coins){
-		for(int i = 0; i < coins.size(); i++){
-			System.out.println(coins.get(i));
-		}
-	}
-	
-	/**
-	 * Return a list of Valuable that cotains only the coins from coins(the parameter).
+	 * Return a list of stuff that implements Valuable that contains 
+	 * only the currency from money(the parameter).
 	 * @param money is list of value.
 	 * @param currency is value's currency.
-	 * @return list of value that have same currency.
+	 * @return list of stuff that implements Valuable that have same currency.
 	 */
-	public static List<Valuable> filterByCurrency(List<Valuable> money, String currency){
-		List<Valuable> coinFilter = new ArrayList<>();
+	public static <E extends Valuable> List<E> filterByCurrency(List<E> money, String currency){
+		List<E> coinFilter = new ArrayList<>();
 		for(int i = 0; i < money.size(); i++){
 			if(money.get(i).getCurrency().equals(currency)){
 				coinFilter.add(money.get(i));
@@ -68,16 +32,45 @@ public class MoneyUtil {
 	}
 	
 	/**
-	 * Sort a list of value and print the result.
-	 * @param coins is list of value.
+	 * Sort a list of value.
+	 * @param money is list of value.
 	 */
-	public static void sortCoins(List<Valuable> coins){
+	public static void sortMoney(List<? extends Valuable> money){
 		Comparator<Valuable> comp = new ValueComparator();
-		java.util.Collections.sort(coins, comp);
-		printCoins(coins);
+		java.util.Collections.sort(money, comp);
+	}
+	
+	/**
+	 * Return the larger argument, based on sort oder, using
+	 * the objects' own compareTo method for comparing.
+	 * @param args one or more Comparable objects to compare.
+	 * @return the argument that would be last if sorted the elements.
+	 * @throws IllegalArgumentException if no arguments given. 
+	 */
+	public static <E extends Comparable<? super E>> E max(E ... args) {
+		if(args == null || args.length == 0) throw new IllegalArgumentException();
+		E max = args[0];
+		for(int i = 0; i < args.length; i++) {
+			max = (args[i].compareTo(max) > 0) ? args[i]:max; 
+		}
+		return max;
 	}
 	
 	public static void main(String[] args) {
-		sortCoins();
+		Money m1 = new BankNote(100, "Baht", 1);
+		Money m2 = new BankNote(500, "Baht", 2);
+		Money m3 = new Coin(20, "Baht");
+		Money max = MoneyUtil.max(m1, m2, m3);
+		System.out.println(max);
+		System.out.println("**********");
+		List<BankNote> list = new ArrayList<BankNote>();
+		list.add(new BankNote(10.0, "USD",1));
+		list.add(new BankNote(500.0, "Baht", 1));
+		MoneyUtil.sortMoney(list);
+		System.out.println(list);
+		System.out.println("**********");
+		List<Coin> coins = Arrays.asList(new Coin(5, "Baht"), new Coin(0.1, "Rinngit"), new Coin(5, "Cent"));
+		List<Coin> result = MoneyUtil.filterByCurrency(coins, "Baht");
+		System.out.println(result);
 	}
 }
